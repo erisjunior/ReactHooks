9d2a98d162b414df687e43791d91dbdfab523a9e
Erisvan Junior <erisvan.junior.a@gmail.com>
2019-06-23 03:30:53 -0300

    Init hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  /* Setar variaveis de estado */
+  const [repositories, setRepositories] = useState([])
+  const [location, setLocations] = useState({})
+
+  /* Component Did Mount */
+  useEffect(() => {
+    fetchData()
+
+    const watch = navigator.geolocation.watchPosition(handlePostionReceived)
+
+    /* Component Will Unmount */
+    return () => navigator.geolocation.clearWatch(watch)
+  }, [])
+
+  /* Do async to fetch outside de useEffect */
+  async function fetchData() {
+    const response = await fetch(
+      'https://api.github.com/users/erisjunior/repos'
+    )
+
+    const data = await response.json()
+
+    setRepositories(data)
+  }
+
+  function handlePostionReceived({ coords }) {
+    const { latitude, longitude } = coords
+
+    setLocations({ latitude, longitude })
+  }
+
+  /* Component Did Update whith repositories */
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite)
+
+    document.title = `Você tem ${filtered.length} favs`
+  }, [repositories])
+
+  function handleAddRepository() {
+    setRepositories([...repositories, { id: Math.random(), name: 'Add' }])
+  }
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    })
+
+    setRepositories(newRepositories)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.name} {repo.favorite && '(Fav)'}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => handleAddRepository}>ADD</button>
+      <p>
+        Lat: {location.latitude} <br />
+        Long: {location.longitude}
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
